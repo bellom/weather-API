d096e55a78ebea62f52fc79a64e14cdd6a6039e4
bellom <bellomsean@gmail.com>
2019-09-05 12:26:49 +0100

    refactoring

--- src/fetchData.js
+++ src/fetchData.js
@@ -5,6 +5,7 @@ const fetchData = () => {
   let location = document.getElementById("city");
   let city = location.value;
   let fetchUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=fa1c7f10e6e75b7dc652e684202b5bd1`;
+  let displayLocation = document.querySelector(".location");
 
   fetch(fetchUrl, { mode: "cors" })
     .then(response => {
@@ -16,20 +17,19 @@ const fetchData = () => {
       const { deg } = response.wind;
       const { country } = response.sys;
 
-      let displayLocation = document.querySelector(".location");
       displayLocation.textContent = `${cityName}, ${country}`;
 
       let loadTemp = document.querySelector(".loadTemp");
       let fahren = Math.round((temp * 9) / (5 + 32));
       let celsius = Math.round(((fahren - 32) * 5) / 9);
-      loadTemp.textContent = fahren;
       let statusF = document.getElementById("f");
       let statusC = document.getElementById("c");
       let loadLetter = document.querySelector(".loadLetter");
       let loadHumidity = document.querySelector(".humidity");
       let loadPressure = document.querySelector(".pressure");
       let loadWind = document.querySelector(".wind");
-
+      
+      loadTemp.textContent = fahren;
       loadHumidity.textContent = humidity;
       loadPressure.textContent = Math.round(pressure);
       loadWind.textContent = Math.round(deg);
@@ -45,7 +45,6 @@ const fetchData = () => {
       });
     })
     .catch(err => {
-      let displayLocation = document.querySelector(".location");
       displayLocation.textContent = "Location Not Found !!";
     });
 };
