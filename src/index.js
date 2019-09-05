64608b6affbaf908584c6101b285ee15bd085fc2
bellom <bellomsean@gmail.com>
2019-09-05 11:05:11 +0100

    refractoring

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import fetchData from "./fetchData";
+// import loadDOM from "./loadDOM";
 
 
 document.addEventListener('click', (e) => {
     if(e.target.id === "checkBtn"){
+        // loadDOM();
         fetchData();
     }
 });
