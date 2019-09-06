38c84475eeb65fffa99d010bb55b92c29f98b68b
bellom <bellomsean@gmail.com>
2019-09-06 22:57:17 +0100

    updated refractoring

--- src/fetchData.js
+++ src/fetchData.js
@@ -1,14 +1,13 @@
-import loadDOM from "./loadDOM";
-
 /* eslint-disable no-redeclare */
 /* global, fetch document */
+
+import { loadDOM, displayErr } from "./loadDOM";
 
 const fetchData = () => {
   let location = document.getElementById("city");
   let city = location.value;
   let fetchUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=fa1c7f10e6e75b7dc652e684202b5bd1`;
-  let displayLocation = document.querySelector(".location");
-
+  
   fetch(fetchUrl, { mode: "cors" })
     .then(response => {
       return response.json();
@@ -22,33 +21,9 @@ const fetchData = () => {
       
       loadDOM (temp, pressure, humidity, cityName, country, deg, fahren);
 
-      // const loadTemp = document.querySelector(".loadTemp");
-      // const celsius = Math.round(((fahren - 32) * 5) / 9);
-      // const statusF = document.getElementById("f");
-      // const statusC = document.getElementById("c");
-      // const loadLetter = document.querySelector(".loadLetter");
-      // const loadHumidity = document.querySelector(".humidity");
-      // const loadPressure = document.querySelector(".pressure");
-      // const loadWind = document.querySelector(".wind");
-      
-      // displayLocation.textContent = `${cityName}, ${country}`;
-      // loadTemp.textContent = fahren;
-      // loadHumidity.textContent = humidity;
-      // loadPressure.textContent = Math.round(pressure);
-      // loadWind.textContent = Math.round(deg);
-
-      // statusF.addEventListener("click", () => {
-      //   loadTemp.textContent = fahren;
-      //   loadLetter.textContent = "F";
-      // });
-
-      // statusC.addEventListener("click", () => {
-      //   loadTemp.textContent = celsius;
-      //   loadLetter.textContent = "C";
-      // });
     })
     .catch((err) => {
-      displayLocation.textContent = "Location Not Found !!";
+      displayErr();
       return err;
     });
 };
--- src/loadDOM.js
+++ src/loadDOM.js
@@ -1,16 +1,16 @@
 /* global, fetch document */
 
+const displayLocation = document.querySelector(".location");
+const loadTemp = document.querySelector(".loadTemp");
+const loadHumidity = document.querySelector(".humidity");
+const loadPressure = document.querySelector(".pressure");
+const loadWind = document.querySelector(".wind");
+const statusF = document.getElementById("f");
+const statusC = document.getElementById("c");
+const loadLetter = document.querySelector(".loadLetter");
+
 const loadDOM = (temp, pressure, humidity, cityName, country, deg, fahren) => {
-  
-  const loadTemp = document.querySelector(".loadTemp");
   const celsius = Math.round(((fahren - 32) * 5) / 9);
-  const statusF = document.getElementById("f");
-  const statusC = document.getElementById("c");
-  const loadLetter = document.querySelector(".loadLetter");
-  const loadHumidity = document.querySelector(".humidity");
-  const loadPressure = document.querySelector(".pressure");
-  const loadWind = document.querySelector(".wind");
-  const displayLocation = document.querySelector(".location");
 
   displayLocation.textContent = `${cityName}, ${country}`;
   loadTemp.textContent = fahren;
@@ -29,4 +29,23 @@ const loadDOM = (temp, pressure, humidity, cityName, country, deg, fahren) => {
   });
 };
 
-export default loadDOM;
+const displayErr = () => {
+  displayLocation.textContent = "";
+  loadTemp.textContent = "0";
+  loadHumidity.textContent = "0";
+  loadPressure.textContent = "0";
+  loadWind.textContent = "0";
+  displayLocation.textContent = "Location Not Found !!";
+
+  statusF.addEventListener("click", () => {
+    loadTemp.textContent = "0";
+    loadLetter.textContent = "F";
+  });
+
+  statusC.addEventListener("click", () => {
+    loadTemp.textContent = "0";
+    loadLetter.textContent = "C";
+  });
+};
+
+export { loadDOM, displayErr };
